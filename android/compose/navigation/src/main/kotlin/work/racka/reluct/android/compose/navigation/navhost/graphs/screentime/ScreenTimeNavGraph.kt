602ca4e16047cef11fe58b93c9cfc0b65d295b82
racka98 <bmkilaha@gmail.com>
2022-06-04 23:02:27 +0300

    setup screen time destination in navgraph

--- android/compose/navigation/src/main/kotlin/work/racka/reluct/android/compose/navigation/navhost/graphs/screentime/ScreenTimeNavGraph.kt
+++ android/compose/navigation/src/main/kotlin/work/racka/reluct/android/compose/navigation/navhost/graphs/screentime/ScreenTimeNavGraph.kt
@@ -1,105 +1,159 @@
 package work.racka.reluct.android.compose.navigation.navhost.graphs.screentime
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraphBuilder
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.navigation
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
-import timber.log.Timber
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import work.racka.reluct.android.compose.components.tab.screentime.ScreenTimeTabBar
+import work.racka.reluct.android.compose.components.topBar.ReluctContentTopBar
+import work.racka.reluct.android.compose.components.util.BarsVisibility
 import work.racka.reluct.android.compose.destinations.ScreenTimeDestinations
 import work.racka.reluct.android.compose.destinations.navbar.Graphs
+import work.racka.reluct.android.compose.navigation.transitions.scaleInEnterTransition
+import work.racka.reluct.android.compose.navigation.transitions.scaleInPopEnterTransition
+import work.racka.reluct.android.compose.navigation.transitions.scaleOutExitTransition
+import work.racka.reluct.android.compose.navigation.transitions.scaleOutPopExitTransition
+import work.racka.reluct.android.screens.screentime.statistics.ScreenTimeStatisticsScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @ExperimentalAnimationApi
-internal fun NavGraphBuilder.screenTimeNavGraph(
-    navController: NavHostController,
+@Composable
+internal fun ScreenTimeNavHost(
+    mainNavController: NavHostController,
+    barsVisibility: BarsVisibility,
+    navController: NavHostController = rememberAnimatedNavController(),
+    mainScaffoldPadding: PaddingValues,
 ) {
-    navigation(
-        route = Graphs.ScreenTimeDestinations.route,
-        startDestination = ScreenTimeDestinations.Statistics.route
-    ) {
-        Timber.d("Statistics screen called")
-        // Statistics
-        composable(
-            route = ScreenTimeDestinations.Statistics.route
-        ) {
-            val tabPage = remember {
-                mutableStateOf(ScreenTimeDestinations.Statistics)
-            }
-            Scaffold(
-                topBar = {
-                    LazyRow {
-                        item {
-                            ScreenTimeTabBar(
-                                modifier = Modifier
-                                    .statusBarsPadding(),
-                                tabPage = tabPage.value,
-                                onTabSelected = { tabPage.value = it }
-                            )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    val tabPage by remember(currentDestination) {
+        derivedStateOf {
+            getCurrentTab(currentDestination)
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            ScreenTimeTopBar(
+                tabPage = tabPage,
+                profilePicUrl = "https://pbs.twimg.com/profile_images/1451052243067805698/LIEt076e_400x400.jpg",
+                updateTabPage = {
+                    navController.navigate(it.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
                         }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                }
-            ) { innerPadding ->
-                val listState = rememberLazyListState()
+                },
+            )
+        }
+    ) { innerPapping ->
+        AnimatedNavHost(
+            modifier = Modifier.padding(innerPapping),
+            navController = navController,
+            route = Graphs.RootDestinations.route + Graphs.ScreenTimeDestinations.route,
+            startDestination = ScreenTimeDestinations.Statistics.route
+        ) {
+            // Statistics
+            composable(
+                route = ScreenTimeDestinations.Statistics.route,
+                // Transition animations
+                enterTransition = { scaleInEnterTransition() },
+                exitTransition = { scaleOutExitTransition() },
+                popEnterTransition = { scaleInPopEnterTransition() },
+                popExitTransition = { scaleOutPopExitTransition() }
+            ) {
+                ScreenTimeStatisticsScreen(
+                    mainScaffoldPadding = mainScaffoldPadding,
+                    barsVisibility = barsVisibility,
+                    onNavigateToAppUsageInfo = { /* TODO: Setup App Info Screen */ }
+                )
+            }
+
+            // Limits
+            composable(
+                route = ScreenTimeDestinations.Limits.route,
+                // Transition animations
+                enterTransition = { scaleInEnterTransition() },
+                exitTransition = { scaleOutExitTransition() },
+                popEnterTransition = { scaleInPopEnterTransition() },
+                popExitTransition = { scaleOutPopExitTransition() }
+            ) {
                 Box(
-                    modifier = Modifier
-                        .padding(innerPadding)
-                        .fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        text = "Tasks: $route",
+                        text = "Screen Time: $route",
                         color = MaterialTheme.colorScheme.onBackground
                     )
-                    /*LazyColumn(
-                        Modifier
-                            .fillMaxWidth(.9f),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        state = listState
-                    ) {
-                        item {
-                            ComponentsPreview2()
-                        }
-
-                        item {
-                            Text(
-                                text = "Tasks: $route",
-                                color = MaterialTheme.colorScheme.onBackground
-                            )
-                        }
-                    }*/
                 }
             }
         }
+    }
+}
 
-        // Limits
-        composable(
-            route = ScreenTimeDestinations.Limits.route
+@Composable
+private fun ScreenTimeTopBar(
+    tabPage: ScreenTimeDestinations,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    profilePicUrl: String?,
+    updateTabPage: (ScreenTimeDestinations) -> Unit,
+) {
+    ReluctContentTopBar(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .statusBarsPadding(),
+        minShrinkHeight = 40.dp,
+        scrollBehavior = scrollBehavior
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement
+                .spacedBy(16.dp)
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Screen Time: $route",
-                    color = MaterialTheme.colorScheme.onBackground
-                )
+            // TODO: Put Prof Pic and Destination Name
+
+            LazyRow {
+                item {
+                    ScreenTimeTabBar(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp),
+                        tabPage = tabPage,
+                        onTabSelected = {
+                            updateTabPage(it)
+                        }
+                    )
+                }
             }
         }
     }
+}
+
+private fun getCurrentTab(currentDestination: NavDestination?): ScreenTimeDestinations {
+    val destinations = ScreenTimeDestinations.values()
+    destinations.forEach { item ->
+        val isSelected = currentDestination?.hierarchy?.any {
+            it.route == item.route
+        } ?: false
+        if (isSelected) return item
+    }
+    return destinations.first()
 }
