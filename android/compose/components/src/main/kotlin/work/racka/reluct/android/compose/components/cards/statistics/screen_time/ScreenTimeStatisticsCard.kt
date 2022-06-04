f7e2e06d2d5fcefb4f546d41a154d7466b31927b
racka98 <bmkilaha@gmail.com>
2022-06-04 20:08:39 +0300

    add ScreenTimeStatisticsCard

---
+++ android/compose/components/src/main/kotlin/work/racka/reluct/android/compose/components/cards/statistics/screen_time/ScreenTimeStatisticsCard.kt
@@ -0,0 +1,111 @@
+package work.racka.reluct.android.compose.components.cards.statistics.screen_time
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import work.racka.reluct.android.compose.components.R
+import work.racka.reluct.android.compose.components.cards.statistics.StatisticsBarChartCard
+import work.racka.reluct.android.compose.components.cards.statistics.StatisticsChartState
+import work.racka.reluct.barChart.BarChartData
+import work.racka.reluct.common.model.domain.usagestats.UsageStats
+import work.racka.reluct.common.model.util.time.Week
+
+@Composable
+fun ScreenTimeStatisticsCard(
+    modifier: Modifier = Modifier,
+    barChartState: StatisticsChartState<Map<Week, UsageStats>>,
+    barColor: Color = MaterialTheme.colorScheme.secondary,
+    selectedDayText: String,
+    selectedDayScreenTime: String,
+    weeklyTotalScreenTime: String,
+    selectedDayIsoNumber: Int,
+    onBarClicked: (Int) -> Unit,
+    weekUpdateButton: @Composable () -> Unit,
+) {
+
+    val topTextAlpha = remember {
+        Animatable(0f)
+    }
+
+    val totalWeekTaskCountAlpha = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(selectedDayText, selectedDayScreenTime) {
+        topTextAlpha.animateTo(0f)
+        topTextAlpha.animateTo(1f)
+    }
+
+    LaunchedEffect(weeklyTotalScreenTime) {
+        totalWeekTaskCountAlpha.animateTo(0f)
+        totalWeekTaskCountAlpha.animateTo(1f)
+    }
+
+    val bars = remember(barChartState) {
+        derivedStateOf {
+            val tempList = mutableListOf<BarChartData.Bar>()
+            barChartState.data.forEach { entry ->
+                tempList.add(
+                    BarChartData.Bar(
+                        value = entry.value.totalScreenTime.toFloat(),
+                        color = barColor,
+                        label = entry.key.dayAcronym,
+                        uniqueId = entry.key.isoDayNumber
+                    )
+                )
+            }
+            tempList.toList()
+        }
+    }
+
+    StatisticsBarChartCard(
+        modifier = modifier,
+        bars = bars.value,
+        dataLoading = barChartState is StatisticsChartState.Loading,
+        selectedDayIsoNumber = selectedDayIsoNumber,
+        onBarClicked = { onBarClicked(it) },
+        topLeftText = {
+            Text(
+                text = selectedDayText,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = LocalContentColor.current
+                    .copy(alpha = topTextAlpha.value)
+            )
+        },
+        topRightText = {
+            Text(
+                text = selectedDayScreenTime,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+                    .copy(alpha = topTextAlpha.value)
+            )
+        },
+        belowChartText = {
+            Spacer(modifier = Modifier)
+            Text(
+                text = stringResource(R.string.weekly_screen_time_tally, weeklyTotalScreenTime),
+                style = MaterialTheme.typography.titleLarge
+                    .copy(fontWeight = FontWeight.Medium),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = LocalContentColor.current
+            )
+        },
+        bodyContent = weekUpdateButton
+    )
+}
