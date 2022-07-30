49bc5b42b75157f7e2757761c506e4a5a8a980fb
racka98 <bmkilaha@gmail.com>
2022-07-30 17:18:56 +0300

    add ManageAppsDialog

--- android/screens/src/main/kotlin/work/racka/reluct/android/screens/screentime/components/ManageAppsUI.kt
+++ android/screens/src/main/kotlin/work/racka/reluct/android/screens/screentime/components/ManageAppsUI.kt
@@ -3,6 +3,8 @@ package work.racka.reluct.android.screens.screentime.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -10,11 +12,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material.icons.rounded.KeyboardDoubleArrowDown
 import androidx.compose.material.icons.rounded.KeyboardDoubleArrowUp
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,11 +23,62 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.window.Dialog
+import work.racka.reluct.android.compose.components.buttons.ReluctButton
 import work.racka.reluct.android.compose.components.cards.headers.ListGroupHeadingHeader
 import work.racka.reluct.android.compose.theme.Dimens
 import work.racka.reluct.android.compose.theme.Shapes
 import work.racka.reluct.android.screens.R
 import work.racka.reluct.common.model.domain.app_info.AppInfo
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ManageAppsDialog(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+    topItemsHeading: String,
+    bottomItemsHeading: String,
+    topItems: List<AppInfo>,
+    bottomItems: List<AppInfo>,
+    onTopItemClicked: (app: AppInfo) -> Unit,
+    onBottomItemClicked: (app: AppInfo) -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Scaffold(
+            modifier = modifier,
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            bottomBar = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = Dimens.MediumPadding.size),
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+                    ReluctButton(
+                        buttonText = stringResource(id = R.string.ok),
+                        icon = Icons.Rounded.Done,
+                        onButtonClicked = onDismiss
+                    )
+                }
+            }
+        ) { paddingValues ->
+            ManageAppsUI(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .padding(Dimens.MediumPadding.size),
+                topItemsHeading = topItemsHeading,
+                bottomItemsHeading = bottomItemsHeading,
+                topItems = topItems,
+                bottomItems = bottomItems,
+                onTopItemClicked = onTopItemClicked,
+                onBottomItemClicked = onBottomItemClicked
+            )
+        }
+    }
+}
 
 /** This is a LazyColum, be careful when nesting it inside other Columns **/
 @OptIn(ExperimentalFoundationApi::class)
