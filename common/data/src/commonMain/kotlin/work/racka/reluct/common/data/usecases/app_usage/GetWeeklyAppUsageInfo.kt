041076483f0528d76fc6d5eaad3e3d0c0adf4044
racka98 <bmkilaha@gmail.com>
2022-07-31 13:58:37 +0300

    update GetWeeklyAppUsageInfo

--- common/data/src/commonMain/kotlin/work/racka/reluct/common/data/usecases/app_usage/GetWeeklyAppUsageInfo.kt
+++ common/data/src/commonMain/kotlin/work/racka/reluct/common/data/usecases/app_usage/GetWeeklyAppUsageInfo.kt
@@ -3,5 +3,5 @@ package work.racka.reluct.common.data.usecases.app_usage
 import work.racka.reluct.common.model.domain.usagestats.AppUsageStats
 
 interface GetWeeklyAppUsageInfo {
-    suspend operator fun invoke(weekOffset: Int, packageName: String): List<AppUsageStats>
+    suspend fun invoke(weekOffset: Int, packageName: String): List<AppUsageStats>
 }
