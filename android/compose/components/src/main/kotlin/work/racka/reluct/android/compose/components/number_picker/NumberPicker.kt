7acb0398477f96775c4e838f5057cb92fdfbdfba
racka98 <bmkilaha@gmail.com>
2022-08-01 10:48:02 +0300

    add NumberPicker

---
+++ android/compose/components/src/main/kotlin/work/racka/reluct/android/compose/components/number_picker/NumberPicker.kt
@@ -0,0 +1,31 @@
+package work.racka.reluct.android.compose.components.number_picker
+
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun NumberPicker(
+    modifier: Modifier = Modifier,
+    label: (Int) -> String = {
+        it.toString()
+    },
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    dividersColor: Color = MaterialTheme.colors.primary,
+    range: Iterable<Int>,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    ListItemPicker(
+        modifier = modifier,
+        label = label,
+        value = value,
+        onValueChange = onValueChange,
+        dividersColor = dividersColor,
+        list = range.toList(),
+        textStyle = textStyle
+    )
+}
