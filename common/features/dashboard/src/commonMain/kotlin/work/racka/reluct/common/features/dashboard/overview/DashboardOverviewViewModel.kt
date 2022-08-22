6013853fb9d408948902de1e88a5c5e2ca3a1ba3
racka98 <bmkilaha@gmail.com>
2022-08-22 13:04:28 +0300

    setup screentime logic in viewmodel

--- common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
+++ common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import work.racka.common.mvvm.viewmodel.CommonViewModel
+import work.racka.reluct.common.data.usecases.app_usage.GetDailyUsageStats
 import work.racka.reluct.common.data.usecases.tasks.GetTasksUseCase
 import work.racka.reluct.common.data.usecases.tasks.ModifyTaskUseCase
 import work.racka.reluct.common.features.dashboard.overview.states.DashboardEvents
@@ -12,10 +13,12 @@ import work.racka.reluct.common.features.dashboard.overview.states.DashboardStat
 import work.racka.reluct.common.features.dashboard.overview.states.TodayScreenTimeState
 import work.racka.reluct.common.features.dashboard.overview.states.TodayTasksState
 import work.racka.reluct.common.model.domain.tasks.Task
+import work.racka.reluct.common.model.util.time.WeekUtils
 
 class DashboardOverviewViewModel(
     private val getTasksUseCase: GetTasksUseCase,
-    private val modifyTasksUsesCase: ModifyTaskUseCase
+    private val modifyTasksUsesCase: ModifyTaskUseCase,
+    private val getDailyUsageStats: GetDailyUsageStats,
 ) : CommonViewModel() {
 
     private val todayScreenTimeState: MutableStateFlow<TodayScreenTimeState> =
@@ -40,9 +43,18 @@ class DashboardOverviewViewModel(
     val events: Flow<DashboardEvents> = _events.receiveAsFlow()
 
     private var pendingTasksJob: Job? = null
+    private var dailyScreenTimeJob: Job? = null
+    private var permissionsJob: Job? = null
+
+    private val permissionGranted = MutableStateFlow(false)
 
     init {
+        initializeScreenTimeData()
         getPendingTasks()
+    }
+
+    fun permissionCheck(isGranted: Boolean) {
+        permissionGranted.update { isGranted }
     }
 
     fun toggleDone(task: Task, isDone: Boolean) {
@@ -59,6 +71,31 @@ class DashboardOverviewViewModel(
             // We get only 5 pending Tasks
             getTasksUseCase.getPendingTasks(factor = 1L, limitBy = 5).collectLatest { tasks ->
                 todayTasksState.update { TodayTasksState.Data(tasks = tasks) }
+            }
+        }
+    }
+
+    private fun initializeScreenTimeData() {
+        permissionsJob?.cancel()
+        permissionsJob = vmScope.launch {
+            permissionGranted.collectLatest { isGranted ->
+                if (isGranted) {
+                    getDailyScreenTime()
+                }
+            }
+        }
+    }
+
+    private fun getDailyScreenTime() {
+        dailyScreenTimeJob?.cancel()
+        todayScreenTimeState.update { TodayScreenTimeState.Loading(usageStats = it.usageStats) }
+        dailyScreenTimeJob = vmScope.launch {
+            val today = WeekUtils.currentDayOfWeek()
+            val dailyData = getDailyUsageStats.invoke(dayIsoNumber = today.isoDayNumber)
+            todayScreenTimeState.update {
+                TodayScreenTimeState.Data(
+                    dailyUsageStats = dailyData
+                )
             }
         }
     }
