9c0b8d3982f3581653795451c825383646e307d4
racka98 <bmkilaha@gmail.com>
2022-08-22 12:38:50 +0300

    add DashboardEvents

--- common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
+++ common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
@@ -1,9 +1,11 @@
 package work.racka.reluct.common.features.dashboard.overview
 
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import work.racka.common.mvvm.viewmodel.CommonViewModel
 import work.racka.reluct.common.data.usecases.tasks.GetTasksUseCase
 import work.racka.reluct.common.data.usecases.tasks.ModifyTaskUseCase
+import work.racka.reluct.common.features.dashboard.overview.states.DashboardEvents
 import work.racka.reluct.common.features.dashboard.overview.states.DashboardState
 import work.racka.reluct.common.features.dashboard.overview.states.TodayScreenTimeState
 import work.racka.reluct.common.features.dashboard.overview.states.TodayTasksState
@@ -30,4 +32,7 @@ class DashboardOverviewViewModel(
         started = SharingStarted.WhileSubscribed(5_000L),
         initialValue = DashboardState()
     )
+
+    private val _events = Channel<DashboardEvents>(Channel.UNLIMITED)
+    val events: Flow<DashboardEvents> = _events.receiveAsFlow()
 }
