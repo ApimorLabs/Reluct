0596f081d9771969c2806c982e6ff85f41f0977d
racka98 <bmkilaha@gmail.com>
2022-08-22 12:48:22 +0300

    setup pending tasks in viewmodel

--- common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
+++ common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
@@ -1,7 +1,9 @@
 package work.racka.reluct.common.features.dashboard.overview
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import work.racka.common.mvvm.viewmodel.CommonViewModel
 import work.racka.reluct.common.data.usecases.tasks.GetTasksUseCase
 import work.racka.reluct.common.data.usecases.tasks.ModifyTaskUseCase
@@ -9,6 +11,7 @@ import work.racka.reluct.common.features.dashboard.overview.states.DashboardEven
 import work.racka.reluct.common.features.dashboard.overview.states.DashboardState
 import work.racka.reluct.common.features.dashboard.overview.states.TodayScreenTimeState
 import work.racka.reluct.common.features.dashboard.overview.states.TodayTasksState
+import work.racka.reluct.common.model.domain.tasks.Task
 
 class DashboardOverviewViewModel(
     private val getTasksUseCase: GetTasksUseCase,
@@ -35,4 +38,28 @@ class DashboardOverviewViewModel(
 
     private val _events = Channel<DashboardEvents>(Channel.UNLIMITED)
     val events: Flow<DashboardEvents> = _events.receiveAsFlow()
+
+    private var pendingTasksJob: Job? = null
+
+    init {
+        getPendingTasks()
+    }
+
+    fun toggleDone(task: Task, isDone: Boolean) {
+        vmScope.launch {
+            modifyTasksUsesCase.toggleTaskDone(task, isDone)
+            _events.send(DashboardEvents.ShowMessageDone(isDone, task.title))
+        }
+    }
+
+    private fun getPendingTasks() {
+        pendingTasksJob?.cancel()
+        todayTasksState.update { TodayTasksState.Loading(it.pending) }
+        pendingTasksJob = vmScope.launch {
+            // We get only 5 pending Tasks
+            getTasksUseCase.getPendingTasks(factor = 1L, limitBy = 5).collectLatest { tasks ->
+                todayTasksState.update { TodayTasksState.Data(tasks = tasks) }
+            }
+        }
+    }
 }
