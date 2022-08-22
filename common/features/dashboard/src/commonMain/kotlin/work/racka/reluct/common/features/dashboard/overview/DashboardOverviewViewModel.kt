67d219bf16df7b5d20383749de88a6b84741b214
racka98 <bmkilaha@gmail.com>
2022-08-22 12:33:51 +0300

    setup ui state in viewmodel

--- common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
+++ common/features/dashboard/src/commonMain/kotlin/work/racka/reluct/common/features/dashboard/overview/DashboardOverviewViewModel.kt
@@ -1,4 +1,33 @@
 package work.racka.reluct.common.features.dashboard.overview
 
-class DashboardOverviewViewModel {
+import kotlinx.coroutines.flow.*
+import work.racka.common.mvvm.viewmodel.CommonViewModel
+import work.racka.reluct.common.data.usecases.tasks.GetTasksUseCase
+import work.racka.reluct.common.data.usecases.tasks.ModifyTaskUseCase
+import work.racka.reluct.common.features.dashboard.overview.states.DashboardState
+import work.racka.reluct.common.features.dashboard.overview.states.TodayScreenTimeState
+import work.racka.reluct.common.features.dashboard.overview.states.TodayTasksState
+
+class DashboardOverviewViewModel(
+    private val getTasksUseCase: GetTasksUseCase,
+    private val modifyTasksUsesCase: ModifyTaskUseCase
+) : CommonViewModel() {
+
+    private val todayScreenTimeState: MutableStateFlow<TodayScreenTimeState> =
+        MutableStateFlow(TodayScreenTimeState.Nothing)
+    private val todayTasksState: MutableStateFlow<TodayTasksState> =
+        MutableStateFlow(TodayTasksState.Nothing)
+
+    val uiState: StateFlow<DashboardState> = combine(
+        todayScreenTimeState, todayTasksState
+    ) { todayScreenTimeState, todayTasksState ->
+        DashboardState(
+            todayScreenTimeState = todayScreenTimeState,
+            todayTasksState = todayTasksState
+        )
+    }.stateIn(
+        scope = vmScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = DashboardState()
+    )
 }
