56f1c6344c339d85705dd82205e61bae1618ce7f
racka98 <bmkilaha@gmail.com>
2022-09-23 09:34:25 +0300

    add on-boarding preference entries

--- common/persistence/settings/src/commonMain/kotlin/work.racka.reluct.common.settings/MultiplatformSettingsImpl.kt
+++ common/persistence/settings/src/commonMain/kotlin/work.racka.reluct.common.settings/MultiplatformSettingsImpl.kt
@@ -12,13 +12,15 @@ internal class MultiplatformSettingsImpl(
     private val _themeFlow = MutableSharedFlow<Int>(replay = 1)
     private val _focusMode = MutableSharedFlow<Boolean>(replay = 1)
     private val _doNotDisturb = MutableSharedFlow<Boolean>(replay = 1)
+    private val _savedVersionCode = MutableSharedFlow<Int>(replay = 1)
+    private val _onBoardingShown = MutableSharedFlow<Boolean>(replay = 1)
 
-    override val theme: Flow<Int>
-        get() = _themeFlow.asSharedFlow()
-    override val focusMode: Flow<Boolean>
-        get() = _focusMode.asSharedFlow()
-    override val doNoDisturb: Flow<Boolean>
-        get() = _doNotDisturb.asSharedFlow()
+    override val theme: Flow<Int> = _themeFlow.asSharedFlow()
+    override val focusMode: Flow<Boolean> = _focusMode.asSharedFlow()
+    override val doNoDisturb: Flow<Boolean> = _doNotDisturb.asSharedFlow()
+    override val savedVersionCode: Flow<Int> = _savedVersionCode.asSharedFlow()
+    override val onBoardingShown: Flow<Boolean> = _onBoardingShown.asSharedFlow()
+
 
     init {
         initializeSettings()
@@ -31,45 +33,43 @@ internal class MultiplatformSettingsImpl(
     }
 
     override fun saveThemeSettings(value: Int): Boolean {
-        settings.putInt(
-            key = Keys.THEME_OPTION,
-            value = value
-        )
+        settings.putInt(key = Keys.THEME_OPTION, value = value)
         return _themeFlow.tryEmit(readThemeSettings())
     }
 
     override fun saveFocusMode(value: Boolean): Boolean {
-        settings.putBoolean(
-            key = Keys.FOCUS_MODE,
-            value = value
-        )
+        settings.putBoolean(key = Keys.FOCUS_MODE, value = value)
         return _focusMode.tryEmit(readFocusMode())
     }
 
     override fun saveDoNotDisturb(value: Boolean): Boolean {
-        settings.putBoolean(
-            key = Keys.DO_NOT_DISTURB,
-            value = value
-        )
+        settings.putBoolean(key = Keys.DO_NOT_DISTURB, value = value)
         return _doNotDisturb.tryEmit(readDoNotDisturb())
+    }
+
+    override fun saveVersionCode(value: Int): Boolean {
+        settings.putInt(key = Keys.SAVED_VERSION_CODE, value = value)
+        return _savedVersionCode.tryEmit(readSavedVersionCode())
+    }
+
+    override fun saveOnBoardingShown(value: Boolean): Boolean {
+        settings.putBoolean(key = Keys.ON_BOARDING_SHOWN, value = value)
+        return _onBoardingShown.tryEmit(readOnBoardingShown())
     }
 
     // Private Read methods
     private fun readThemeSettings(): Int =
-        settings.getInt(
-            key = Keys.THEME_OPTION,
-            defaultValue = -1
-        )
+        settings.getInt(key = Keys.THEME_OPTION, defaultValue = -1)
 
     private fun readFocusMode(): Boolean =
-        settings.getBoolean(
-            key = Keys.FOCUS_MODE,
-            defaultValue = false
-        )
+        settings.getBoolean(key = Keys.FOCUS_MODE, defaultValue = false)
 
     private fun readDoNotDisturb(): Boolean =
-        settings.getBoolean(
-            key = Keys.DO_NOT_DISTURB,
-            defaultValue = false
-        )
+        settings.getBoolean(key = Keys.DO_NOT_DISTURB, defaultValue = false)
+
+    private fun readSavedVersionCode(): Int =
+        settings.getInt(key = Keys.SAVED_VERSION_CODE, defaultValue = 0)
+
+    private fun readOnBoardingShown(): Boolean =
+        settings.getBoolean(key = Keys.ON_BOARDING_SHOWN, defaultValue = false)
 }
