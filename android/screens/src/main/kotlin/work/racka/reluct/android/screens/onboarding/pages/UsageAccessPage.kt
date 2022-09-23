212f63c96903af7410ff3eab6a0fde9292bf52b8
racka98 <bmkilaha@gmail.com>
2022-09-23 21:41:06 +0300

    add more pages

---
+++ android/screens/src/main/kotlin/work/racka/reluct/android/screens/onboarding/pages/UsageAccessPage.kt
@@ -0,0 +1,138 @@
+package work.racka.reluct.android.screens.onboarding.pages
+
+import android.content.Intent
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import work.racka.reluct.android.compose.components.buttons.ReluctButton
+import work.racka.reluct.android.compose.theme.Dimens
+import work.racka.reluct.android.compose.theme.Shapes
+import work.racka.reluct.android.screens.R
+import work.racka.reluct.android.screens.onboarding.components.PermissionStatusCard
+import work.racka.reluct.android.screens.util.PermissionCheckHandler
+import work.racka.reluct.android.screens.util.checkUsageAccessPermissions
+
+@Composable
+internal fun UsageAccessPage(
+    modifier: Modifier = Modifier,
+    isGranted: Boolean,
+    updatePermissionCheck: (isGranted: Boolean) -> Unit
+) {
+
+    val drawableSize = 400.dp
+    val context = LocalContext.current
+
+    val openDialog = remember { mutableStateOf(false) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = {
+            updatePermissionCheck(checkUsageAccessPermissions(context))
+        }
+    )
+
+    PermissionCheckHandler {
+        if (!isGranted) {
+            updatePermissionCheck(checkUsageAccessPermissions(context))
+        }
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = Dimens.LargePadding.size) then modifier,
+        verticalArrangement = Arrangement.spacedBy(Dimens.MediumPadding.size),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Text(
+                text = stringResource(id = R.string.usage_access_text),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineLarge
+                    .copy(fontSize = 40.sp)
+            )
+        }
+
+        item {
+            Image(
+                modifier = Modifier
+                    .size(drawableSize)
+                    .padding(Dimens.MediumPadding.size),
+                painter = painterResource(id = R.drawable.stats_pie_chart),
+                contentDescription = null
+            )
+        }
+
+        item {
+            Text(
+                text = stringResource(id = R.string.usage_access_desc_text),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+
+        item {
+            PermissionStatusCard(isGranted = isGranted) {
+                if (!isGranted) {
+                    openDialog.value = true
+                }
+            }
+        }
+    }
+
+    // Go To Usage Access Dialog
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = { openDialog.value = false },
+            title = {
+                Text(text = stringResource(R.string.open_settings_dialog_title))
+            },
+            text = {
+                Text(text = stringResource(R.string.usage_permissions_rationale_dialog_text))
+            },
+            confirmButton = {
+                ReluctButton(
+                    buttonText = stringResource(R.string.ok),
+                    icon = null,
+                    shape = Shapes.large,
+                    buttonColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    onButtonClicked = {
+                        openDialog.value = false
+                        launcher.launch(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+                    }
+                )
+            },
+            dismissButton = {
+                ReluctButton(
+                    buttonText = stringResource(R.string.cancel),
+                    icon = null,
+                    shape = Shapes.large,
+                    buttonColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    onButtonClicked = { openDialog.value = false }
+                )
+            }
+        )
+    }
+}
