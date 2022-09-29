aac0936e9841e3a872057920259c8c4a6e70e1ca
racka98 <bmkilaha@gmail.com>
2022-09-29 18:22:36 +0300

    add Goal models

---
+++ common/model/src/commonMain/kotlin/work/racka/reluct/common/model/domain/goals/Goal.kt
@@ -0,0 +1,17 @@
+package work.racka.reluct.common.model.domain.goals
+
+import work.racka.reluct.common.model.domain.app_info.AppInfo
+import work.racka.reluct.common.model.util.time.Week
+
+data class Goal(
+    val id: String,
+    val name: String,
+    val isActive: Boolean,
+    val relatedApps: List<AppInfo>,
+    val targetValue: Long,
+    val currentValue: Long,
+    val goalInterval: GoalInterval,
+    val timeInterval: LongRange?,
+    val daysOfWeekSelected: List<Week> = emptyList(),
+    val goalType: GoalType
+)
