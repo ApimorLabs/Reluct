086c522ba99b2a35251d58e4e1b7d4fc9f8b516c
racka98 <bmkilaha@gmail.com>
2022-10-03 20:04:36 +0300

    add method to sync DeviceScreenTimeGoal type

--- common/data/src/commonMain/kotlin/work/racka/reluct/common/data/usecases/goals/impl/ModifyGoalsImpl.kt
+++ common/data/src/commonMain/kotlin/work/racka/reluct/common/data/usecases/goals/impl/ModifyGoalsImpl.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import work.racka.reluct.common.app.usage.stats.manager.UsageDataManager
 import work.racka.reluct.common.data.mappers.goals.asGoalDbObject
+import work.racka.reluct.common.data.usecases.app_usage.GetUsageStats
 import work.racka.reluct.common.data.usecases.goals.ModifyGoals
 import work.racka.reluct.common.data.usecases.tasks.GetGroupedTasksStats
 import work.racka.reluct.common.database.dao.goals.GoalsDao
@@ -12,15 +14,22 @@ import work.racka.reluct.common.database.models.GoalDbObject
 import work.racka.reluct.common.model.domain.goals.Goal
 import work.racka.reluct.common.model.domain.goals.GoalInterval
 import work.racka.reluct.common.model.domain.goals.GoalType
+import work.racka.reluct.common.model.util.time.StatisticsTimeUtils
+import work.racka.reluct.common.model.util.time.Week
 import work.racka.reluct.common.model.util.time.WeekUtils
 import work.racka.reluct.common.system_service.haptics.HapticFeedback
 
 class ModifyGoalsImpl(
     private val goalsDao: GoalsDao,
     private val haptics: HapticFeedback,
+    private val usageDataManager: UsageDataManager,
     private val getGroupedTasksStats: GetGroupedTasksStats,
+    private val getUsageStats: GetUsageStats,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ModifyGoals {
+
+    private val daysOfWeek get() = Week.values()
+
     override suspend fun saveGoal(goal: Goal) = withContext(dispatcher) {
         goalsDao.insertGoal(goal.asGoalDbObject())
         haptics.click()
@@ -48,7 +57,7 @@ class ModifyGoalsImpl(
         activeGoals.map { goal ->
             when (goal.goalType) {
                 GoalType.TasksGoal -> manageTasksGoalType(goal)
-                GoalType.DeviceScreenTimeGoal -> {}
+                GoalType.DeviceScreenTimeGoal -> manageDeviceScreenTimeGoalType(goal)
                 else -> {}
             }
         }
@@ -85,7 +94,52 @@ class ModifyGoalsImpl(
             }
         }
 
-    private fun manageDeviceScreenTimeGoalType(goal: GoalDbObject) {
-
-    }
+    private suspend fun manageDeviceScreenTimeGoalType(goal: GoalDbObject): GoalDbObject =
+        when (goal.goalInterval) {
+            GoalInterval.Daily -> {
+                val today = WeekUtils.currentDayOfWeek().isoDayNumber
+                val selectedDayTimeRange = StatisticsTimeUtils.selectedDayTimeInMillisRange(
+                    weekOffset = 0,
+                    dayIsoNumber = today
+                )
+                val stats = usageDataManager.getUsageStats(
+                    startTimeMillis = selectedDayTimeRange.first,
+                    endTimeMillis = selectedDayTimeRange.last
+                )
+                goal.copy(currentValue = stats.appsUsageList.sumOf { it.timeInForeground })
+            }
+            GoalInterval.Weekly -> {
+                if (goal.daysOfWeekSelected.isEmpty() || goal.daysOfWeekSelected.size == 7) {
+                    val weekTimeRange = StatisticsTimeUtils.weekTimeInMillisRange()
+                    val stats = usageDataManager.getUsageStats(
+                        startTimeMillis = weekTimeRange.first,
+                        endTimeMillis = weekTimeRange.last
+                    )
+                    goal.copy(currentValue = stats.appsUsageList.sumOf { it.timeInForeground })
+                } else {
+                    var screenTime = 0L
+                    goal.daysOfWeekSelected.map { day ->
+                        val selectedDayTimeRange = StatisticsTimeUtils.selectedDayTimeInMillisRange(
+                            weekOffset = 0,
+                            dayIsoNumber = day.isoDayNumber
+                        )
+                        screenTime += usageDataManager.getUsageStats(
+                            startTimeMillis = selectedDayTimeRange.first,
+                            endTimeMillis = selectedDayTimeRange.last
+                        ).appsUsageList.sumOf { it.timeInForeground }
+                    }
+                    goal.copy(currentValue = screenTime)
+                }
+            }
+            GoalInterval.Custom -> {
+                val timeInterval = goal.timeInterval
+                if (timeInterval != null) {
+                    val stats = usageDataManager.getUsageStats(
+                        timeInterval.first,
+                        timeInterval.last
+                    )
+                    goal.copy(currentValue = stats.appsUsageList.sumOf { it.timeInForeground })
+                } else goal
+            }
+        }
 }
