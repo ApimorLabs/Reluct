82b87379be99cd328dd74eed3db065b645a1c5b9
racka98 <bmkilaha@gmail.com>
2022-10-13 19:13:15 +0300

    update AddEditGoalViewModel

--- common/features/goals/src/commonMain/kotlin/work/racka/reluct/common/features/goals/add_edit_goal/AddEditGoalViewModel.kt
+++ common/features/goals/src/commonMain/kotlin/work/racka/reluct/common/features/goals/add_edit_goal/AddEditGoalViewModel.kt
@@ -1,15 +1,20 @@
 package work.racka.reluct.common.features.goals.add_edit_goal
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.onSuccess
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import work.racka.common.mvvm.viewmodel.CommonViewModel
+import work.racka.reluct.common.data.usecases.app_info.GetInstalledApps
 import work.racka.reluct.common.data.usecases.goals.GetGoals
 import work.racka.reluct.common.data.usecases.goals.ModifyGoals
 import work.racka.reluct.common.features.goals.active.states.DefaultGoals
 import work.racka.reluct.common.features.goals.active.states.GoalsEvents
 import work.racka.reluct.common.features.goals.add_edit_goal.states.AddEditGoalState
+import work.racka.reluct.common.features.goals.add_edit_goal.states.GoalAppsState
+import work.racka.reluct.common.features.goals.add_edit_goal.states.ModifyGoalState
+import work.racka.reluct.common.model.domain.app_info.AppInfo
 import work.racka.reluct.common.model.domain.goals.Goal
 
 /**
@@ -19,16 +24,35 @@ import work.racka.reluct.common.model.domain.goals.Goal
 class AddEditGoalViewModel(
     private val getGoals: GetGoals,
     private val modifyGoals: ModifyGoals,
+    private val getInstalledApps: GetInstalledApps,
     private val goalId: String?,
     private val defaultGoalIndex: Int?
 ) : CommonViewModel() {
 
-    private val state: MutableStateFlow<AddEditGoalState> =
-        MutableStateFlow(AddEditGoalState.Loading)
-    val uiState: StateFlow<AddEditGoalState> = state.asStateFlow()
+    private val goalAppsState: MutableStateFlow<GoalAppsState> =
+        MutableStateFlow(GoalAppsState.Nothing)
+    private val modifyGoalState: MutableStateFlow<ModifyGoalState> =
+        MutableStateFlow(ModifyGoalState.Loading)
+    val uiState: StateFlow<AddEditGoalState> = combine(
+        modifyGoalState, goalAppsState
+    ) { modifyGoalState, goalAppsState ->
+        AddEditGoalState(
+            modifyGoalState = modifyGoalState,
+            unSelectedAppsState = goalAppsState
+        )
+    }.stateIn(
+        scope = vmScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = AddEditGoalState()
+    )
 
     private val eventsChannel = Channel<GoalsEvents>(Channel.UNLIMITED)
     val events: Flow<GoalsEvents> = eventsChannel.receiveAsFlow()
+
+
+    private var installedApps: List<AppInfo> = emptyList()
+
+    private var syncAppsJob: Job? = null
 
     init {
         getGoal(goalId)
@@ -36,8 +60,8 @@ class AddEditGoalViewModel(
 
     fun saveCurrentGoal() {
         vmScope.launch {
-            val goalState = state.value
-            if (goalState is AddEditGoalState.Data) {
+            val goalState = modifyGoalState.value
+            if (goalState is ModifyGoalState.Data) {
                 modifyGoals.saveGoal(goalState.goal)
                 val result =
                     eventsChannel.trySend(GoalsEvents.GoalSavedMessage(goalState.goal.name))
@@ -47,18 +71,50 @@ class AddEditGoalViewModel(
                      * is not null else just show the GoalSaved State for adding more tasks
                      */
                     goalId?.run { eventsChannel.send(GoalsEvents.Exit) }
-                        ?: state.update { AddEditGoalState.Saved }
+                        ?: modifyGoalState.update { ModifyGoalState.Saved }
                 }
             }
         }
     }
 
     fun newGoal() {
-        state.update { AddEditGoalState.Data(isEdit = false, goal = DefaultGoals.emptyGoal()) }
+        modifyGoalState.update {
+            ModifyGoalState.Data(
+                isEdit = false,
+                goal = DefaultGoals.emptyGoal()
+            )
+        }
+    }
+
+    fun syncRelatedApps() {
+        syncAppsJob?.cancel()
+        syncAppsJob = vmScope.launch {
+            goalAppsState.update { GoalAppsState.Loading }
+            if (installedApps.isEmpty()) {
+                installedApps = getInstalledApps.invoke()
+            }
+            val goalState = modifyGoalState.value
+            if (goalState is ModifyGoalState.Data) {
+                val apps = installedApps - goalState.goal.relatedApps.toSet()
+                goalAppsState.update { GoalAppsState.Data(apps) }
+            } else goalAppsState.update { GoalAppsState.Data(installedApps) }
+        }
     }
 
     fun updateCurrentGoal(goal: Goal) {
-        state.update { AddEditGoalState.Data(isEdit = goalId != null, goal = goal) }
+        modifyGoalState.update { ModifyGoalState.Data(isEdit = goalId != null, goal = goal) }
+    }
+
+    fun modifyRelatedApps(appInfo: AppInfo, isAdd: Boolean) {
+        val goalState = modifyGoalState.value
+        if (goalState is ModifyGoalState.Data) {
+            val relatedApps = goalState.goal.relatedApps
+                .toMutableSet()
+                .apply { if (isAdd) add(appInfo) else remove(appInfo) }
+            val unSelectedApps = installedApps - relatedApps
+            goalAppsState.update { GoalAppsState.Data(unSelectedApps) }
+            updateCurrentGoal(goalState.goal.copy(relatedApps = relatedApps.toList()))
+        }
     }
 
     private fun getGoal(id: String?) {
@@ -66,15 +122,20 @@ class AddEditGoalViewModel(
             if (defaultGoalIndex != null) getPredefinedGoal(defaultGoalIndex)
             else
                 when (id) {
-                    null -> state.update {
-                        AddEditGoalState.Data(
+                    null -> modifyGoalState.update {
+                        ModifyGoalState.Data(
                             isEdit = false, goal = DefaultGoals.emptyGoal()
                         )
                     }
                     else -> {
                         when (val goal = getGoals.getGoal(id).firstOrNull()) {
-                            null -> state.update { AddEditGoalState.NotFound }
-                            else -> state.update { AddEditGoalState.Data(goal, isEdit = true) }
+                            null -> modifyGoalState.update { ModifyGoalState.NotFound }
+                            else -> modifyGoalState.update {
+                                ModifyGoalState.Data(
+                                    goal,
+                                    isEdit = true
+                                )
+                            }
                         }
                     }
                 }
@@ -83,8 +144,8 @@ class AddEditGoalViewModel(
 
     private fun getPredefinedGoal(index: Int) {
         val goal = DefaultGoals.predefined().getOrNull(index)
-        state.update {
-            AddEditGoalState.Data(
+        modifyGoalState.update {
+            ModifyGoalState.Data(
                 isEdit = false,
                 goal = goal ?: DefaultGoals.emptyGoal()
             )
--- common/features/goals/src/commonMain/kotlin/work/racka/reluct/common/features/goals/add_edit_goal/states/AddEditGoalState.kt
+++ common/features/goals/src/commonMain/kotlin/work/racka/reluct/common/features/goals/add_edit_goal/states/AddEditGoalState.kt
@@ -1,16 +1,34 @@
 package work.racka.reluct.common.features.goals.add_edit_goal.states
 
+import work.racka.reluct.common.model.domain.app_info.AppInfo
 import work.racka.reluct.common.model.domain.goals.Goal
 
-sealed class AddEditGoalState {
+data class AddEditGoalState(
+    val modifyGoalState: ModifyGoalState = ModifyGoalState.Loading,
+    val unSelectedAppsState: GoalAppsState = GoalAppsState.Nothing
+)
+
+sealed class ModifyGoalState {
     data class Data(
         val goal: Goal,
         val isEdit: Boolean
-    ) : AddEditGoalState()
+    ) : ModifyGoalState()
 
-    object Saved : AddEditGoalState()
+    object Saved : ModifyGoalState()
 
-    object Loading : AddEditGoalState()
+    object Loading : ModifyGoalState()
 
-    object NotFound : AddEditGoalState()
+    object NotFound : ModifyGoalState()
+}
+
+sealed class GoalAppsState(
+    val unselectedApps: List<AppInfo>
+) {
+    object Nothing : GoalAppsState(emptyList())
+
+    data class Data(
+        private val apps: List<AppInfo>
+    ) : GoalAppsState(apps)
+
+    object Loading : GoalAppsState(emptyList())
 }
