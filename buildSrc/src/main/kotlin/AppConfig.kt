74e3a01cf097eb660e7c115fcfe86e2cf7981429
racka98 <bmkilaha@gmail.com>
2022-10-20 14:59:31 +0300

    bump version for play store internal testing

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdkVersion = 33
     const val minSdkVersion = 26
     const val targetSdkVersion = 33
-    const val versionCode = 4
-    const val versionName = "1.0.0-alpha03"
+    const val versionCode = 5
+    const val versionName = "1.0.0-alpha04"
     const val applicationId = "work.racka.reluct"
 
     object Desktop {
