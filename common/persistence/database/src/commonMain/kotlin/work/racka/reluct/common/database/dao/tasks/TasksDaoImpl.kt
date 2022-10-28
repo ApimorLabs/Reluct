3fbc0d31c42a26cec6ddd1c8a964d9ec060a5981
racka98 <bmkilaha@gmail.com>
2022-10-28 14:57:28 +0300

    fix issue where updating labels doesn't update Tasks

--- common/persistence/database/src/commonMain/kotlin/work/racka/reluct/common/database/dao/tasks/TasksDaoImpl.kt
+++ common/persistence/database/src/commonMain/kotlin/work/racka/reluct/common/database/dao/tasks/TasksDaoImpl.kt
@@ -5,22 +5,13 @@ import com.squareup.sqldelight.runtime.coroutines.mapToList
 import com.squareup.sqldelight.runtime.coroutines.mapToOneOrNull
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 import work.racka.reluct.common.database.dao.DatabaseWrapper
 import work.racka.reluct.common.database.dao.tasks.TasksHelpers.asDbObject
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.getAllTasksFromDb
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.getCompletedTasksFromDb
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.getPendingTasksFromDb
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.getTaskFromDb
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.getTasksBetweenDateTimeStringsFromDb
 import work.racka.reluct.common.database.dao.tasks.TasksHelpers.insertAllLabelsToDb
 import work.racka.reluct.common.database.dao.tasks.TasksHelpers.insertLabelToDb
 import work.racka.reluct.common.database.dao.tasks.TasksHelpers.insertTaskToDb
-import work.racka.reluct.common.database.dao.tasks.TasksHelpers.searchTasksFromDb
 import work.racka.reluct.common.database.models.TaskDbObject
 import work.racka.reluct.common.database.models.TaskLabelDbObject
 
@@ -36,66 +27,77 @@ internal class TasksDaoImpl(
         tasksQueries?.insertTaskToDb(task)
     }
 
-    override fun getAllTasks(): Flow<List<TaskDbObject>> =
-        tasksQueries?.getAllTasksFromDb(onGetLabel = ::getTaskLabelSync)
-            ?.asFlow()
-            ?.mapToList(dispatcher)
-            ?: flowOf(emptyList())
-
-    // TODO: Use later
-    fun getTasks(): Flow<List<TaskDbObject>> {
+    override fun getAllTasks(): Flow<List<TaskDbObject>> {
         val dbTasks = tasksQueries?.getAllTasks()?.asFlow()?.mapToList(dispatcher)
             ?: flowOf(emptyList())
         val dbLabels = getAllTaskLabels()
-        return dbTasks.zip(dbLabels) { tasks, labels ->
+        return combine(dbTasks, dbLabels) { tasks, labels ->
             withContext(dispatcher) {
                 tasks.map { task -> task.asDbObject(labels) }
             }
         }
     }
 
+    override fun getTask(id: String): Flow<TaskDbObject?> {
+        val dbTasks = tasksQueries?.getTask(id)?.asFlow()
+            ?.mapToOneOrNull(dispatcher) ?: flowOf(null)
+        val dbLabels = getAllTaskLabels()
+        return combine(dbTasks, dbLabels) { task, labels ->
+            withContext(dispatcher) {
+                task?.asDbObject(labels)
+            }
+        }
+    }
 
-    override fun getTask(id: String): Flow<TaskDbObject?> =
-        tasksQueries?.getTaskFromDb(id, onGetLabel = ::getTaskLabelSync)
-            ?.asFlow()
-            ?.mapToOneOrNull(dispatcher)
-            ?: flowOf(null)
+    override fun searchTasks(query: String, factor: Long, limitBy: Long): Flow<List<TaskDbObject>> {
+        val dbTasks =
+            tasksQueries?.searchTasks(query = "%$query%", factor = factor, limitBy = limitBy)
+                ?.asFlow()?.mapToList(dispatcher) ?: flowOf(emptyList())
+        val dbLabels = getAllTaskLabels()
+        return combine(dbTasks, dbLabels) { tasks, labels ->
+            withContext(dispatcher) {
+                tasks.map { task -> task.asDbObject(labels) }
+            }
+        }
+    }
 
-    override fun searchTasks(query: String, factor: Long, limitBy: Long): Flow<List<TaskDbObject>> =
-        tasksQueries?.searchTasksFromDb(
-            query = "%$query%",
-            factor = factor,
-            limitBy = limitBy,
-            onGetLabel = ::getTaskLabelSync
-        )
-            ?.asFlow()
-            ?.mapToList(dispatcher)
-            ?: flowOf(emptyList())
+    override fun getPendingTasks(factor: Long, limitBy: Long): Flow<List<TaskDbObject>> {
+        val dbTasks = tasksQueries?.getPendingTasks(factor, limitBy)
+            ?.asFlow()?.mapToList(dispatcher) ?: flowOf(emptyList())
+        val dbLabels = getAllTaskLabels()
+        return combine(dbTasks, dbLabels) { tasks, labels ->
+            withContext(dispatcher) {
+                tasks.map { task -> task.asDbObject(labels) }
+            }
+        }
+    }
 
-    override fun getPendingTasks(factor: Long, limitBy: Long): Flow<List<TaskDbObject>> =
-        tasksQueries?.getPendingTasksFromDb(factor, limitBy, onGetLabel = ::getTaskLabelSync)
-            ?.asFlow()
-            ?.mapToList(dispatcher)
-            ?: flowOf(emptyList())
-
-    override fun getCompletedTasks(factor: Long, limitBy: Long): Flow<List<TaskDbObject>> =
-        tasksQueries?.getCompletedTasksFromDb(factor, limitBy, onGetLabel = ::getTaskLabelSync)
-            ?.asFlow()
-            ?.mapToList(dispatcher)
-            ?: flowOf(emptyList())
+    override fun getCompletedTasks(factor: Long, limitBy: Long): Flow<List<TaskDbObject>> {
+        val dbTasks = tasksQueries?.getCompeletedTasks(factor, limitBy)
+            ?.asFlow()?.mapToList(dispatcher) ?: flowOf(emptyList())
+        val dbLabels = getAllTaskLabels()
+        return combine(dbTasks, dbLabels) { tasks, labels ->
+            withContext(dispatcher) {
+                tasks.map { task -> task.asDbObject(labels) }
+            }
+        }
+    }
 
     override fun getTasksBetweenDateTime(
         startLocalDateTime: String,
         endLocalDateTime: String,
-    ): Flow<List<TaskDbObject>> =
-        tasksQueries?.getTasksBetweenDateTimeStringsFromDb(
+    ): Flow<List<TaskDbObject>> {
+        val dbTasks = tasksQueries?.getTasksBetweenDateTimeStrings(
             startLocalDateTime = startLocalDateTime,
             endLocalDateTime = endLocalDateTime,
-            onGetLabel = ::getTaskLabelSync
-        )
-            ?.asFlow()
-            ?.mapToList(dispatcher)
-            ?: flowOf(emptyList())
+        )?.asFlow()?.mapToList(dispatcher) ?: flowOf(emptyList())
+        val dbLabels = getAllTaskLabels()
+        return combine(dbTasks, dbLabels) { tasks, labels ->
+            withContext(dispatcher) {
+                tasks.map { task -> task.asDbObject(labels) }
+            }
+        }
+    }
 
     override fun toggleTaskDone(
         id: String,
--- common/persistence/database/src/commonMain/kotlin/work/racka/reluct/common/database/dao/tasks/TasksHelpers.kt
+++ common/persistence/database/src/commonMain/kotlin/work/racka/reluct/common/database/dao/tasks/TasksHelpers.kt
@@ -28,59 +28,6 @@ internal object TasksHelpers {
         }
     }
 
-    fun TasksTableQueries.getAllTasksFromDb(onGetLabel: (id: String) -> TaskLabelDbObject?) =
-        getAllTasks(mapper = taskDbObjectMapper(onGetLabel))
-
-    fun TasksTableQueries.getPendingTasksFromDb(
-        factor: Long,
-        limitBy: Long,
-        onGetLabel: (id: String) -> TaskLabelDbObject?
-    ) =
-        getPendingTasks(
-            factor = factor,
-            limitBy = limitBy,
-            mapper = taskDbObjectMapper(onGetLabel)
-        )
-
-    fun TasksTableQueries.getCompletedTasksFromDb(
-        factor: Long,
-        limitBy: Long,
-        onGetLabel: (id: String) -> TaskLabelDbObject?
-    ) =
-        getCompeletedTasks(
-            factor = factor,
-            limitBy = limitBy,
-            mapper = taskDbObjectMapper(onGetLabel)
-        )
-
-    fun TasksTableQueries.getTasksBetweenDateTimeStringsFromDb(
-        startLocalDateTime: String,
-        endLocalDateTime: String,
-        onGetLabel: (id: String) -> TaskLabelDbObject?
-    ) = getTasksBetweenDateTimeStrings(
-        startLocalDateTime = startLocalDateTime,
-        endLocalDateTime = endLocalDateTime,
-        mapper = taskDbObjectMapper(onGetLabel)
-    )
-
-    fun TasksTableQueries.getTaskFromDb(
-        taskId: String,
-        onGetLabel: (id: String) -> TaskLabelDbObject?
-    ) = getTask(id = taskId, mapper = taskDbObjectMapper(onGetLabel))
-
-    fun TasksTableQueries.searchTasksFromDb(
-        query: String,
-        factor: Long,
-        limitBy: Long,
-        onGetLabel: (id: String) -> TaskLabelDbObject?
-    ) =
-        searchTasks(
-            query = query,
-            factor = factor,
-            limitBy = limitBy,
-            mapper = taskDbObjectMapper(onGetLabel)
-        )
-
     /**
      * Task Labels
      */
@@ -121,28 +68,6 @@ internal object TasksHelpers {
         TaskLabelDbObject(id = id, name = name, description = description, colorHexString = color)
     }
 
-    private fun taskDbObjectMapper(onGetLabel: (id: String) -> TaskLabelDbObject?): (
-        id: String, title: String, description: String?, done: Boolean, overdue: Boolean,
-        dueDateLocalDateTime: String, completedLocalDateTime: String?,
-        reminderLocalDateTime: String?, timeZoneId: String, taskLabelsId: List<String>
-    ) -> TaskDbObject = {
-            id, title, description, done, overdue, dueDateLocalDateTime,
-            completedLocalDateTime, reminderLocalDateTime, timeZoneId, taskLabelsId,
-        ->
-        TaskDbObject(
-            id = id,
-            title = title,
-            description = description,
-            done = done,
-            overdue = overdue,
-            taskLabels = taskLabelsId.mapNotNull { onGetLabel(it) },
-            dueDateLocalDateTime = dueDateLocalDateTime,
-            completedLocalDateTime = completedLocalDateTime,
-            reminderLocalDateTime = reminderLocalDateTime,
-            timeZoneId = timeZoneId
-        )
-    }
-
     fun TasksTable.asDbObject(allLabels: List<TaskLabelDbObject>) = TaskDbObject(
         id = id,
         title = title,
@@ -157,4 +82,28 @@ internal object TasksHelpers {
         reminderLocalDateTime = reminderLocalDateTime,
         timeZoneId = timeZoneId
     )
+
+    /**
+     * Not In Use Right Now
+    private fun taskDbObjectMapper(onGetLabel: (id: String) -> TaskLabelDbObject?): (
+    id: String, title: String, description: String?, done: Boolean, overdue: Boolean,
+    dueDateLocalDateTime: String, completedLocalDateTime: String?,
+    reminderLocalDateTime: String?, timeZoneId: String, taskLabelsId: List<String>
+    ) -> TaskDbObject = {
+    id, title, description, done, overdue, dueDateLocalDateTime,
+    completedLocalDateTime, reminderLocalDateTime, timeZoneId, taskLabelsId,
+    ->
+    TaskDbObject(
+    id = id,
+    title = title,
+    description = description,
+    done = done,
+    overdue = overdue,
+    taskLabels = taskLabelsId.mapNotNull { onGetLabel(it) },
+    dueDateLocalDateTime = dueDateLocalDateTime,
+    completedLocalDateTime = completedLocalDateTime,
+    reminderLocalDateTime = reminderLocalDateTime,
+    timeZoneId = timeZoneId
+    )
+    }*/
 }
