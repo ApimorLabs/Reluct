71ab79b8f0d013a008e4283cb7e5aca793c611d4
racka98 <bmkilaha@gmail.com>
2022-10-31 22:51:41 +0300

    update GetGoals to ImmutableList
    Signed-off-by: racka98 <bmkilaha@gmail.com>

--- common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/goals/GetGoals.kt
+++ common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/goals/GetGoals.kt
@@ -1,11 +1,12 @@
 package work.racka.reluct.common.domain.usecases.goals
 
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.Flow
 import work.racka.reluct.common.model.domain.goals.Goal
 
 interface GetGoals {
-    fun getActiveGoals(factor: Long, limitBy: Long = 10): Flow<List<Goal>>
-    fun getInActiveGoals(factor: Long, limitBy: Long = 10): Flow<List<Goal>>
+    fun getActiveGoals(factor: Long, limitBy: Long = 10): Flow<ImmutableList<Goal>>
+    fun getInActiveGoals(factor: Long, limitBy: Long = 10): Flow<ImmutableList<Goal>>
     fun getGoal(id: String): Flow<Goal?>
     suspend fun getGoalSync(id: String): Goal?
 }
