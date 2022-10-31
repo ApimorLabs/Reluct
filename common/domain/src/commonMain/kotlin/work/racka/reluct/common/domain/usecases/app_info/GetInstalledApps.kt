8ad1e7403d0af9d0e0e1e1b9c973dbc46ad12eba
racka98 <bmkilaha@gmail.com>
2022-11-01 00:41:14 +0300

    update apps data to ImmutableCollection
    Signed-off-by: racka98 <bmkilaha@gmail.com>

--- common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/app_info/GetInstalledApps.kt
+++ common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/app_info/GetInstalledApps.kt
@@ -1,7 +1,8 @@
 package work.racka.reluct.common.domain.usecases.app_info
 
+import kotlinx.collections.immutable.ImmutableList
 import work.racka.reluct.common.model.domain.appInfo.AppInfo
 
 interface GetInstalledApps {
-    suspend fun invoke(): List<AppInfo>
+    suspend fun invoke(): ImmutableList<AppInfo>
 }
