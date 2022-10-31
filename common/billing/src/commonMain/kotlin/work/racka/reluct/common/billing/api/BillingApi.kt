dc809881ea87d3fdbb697b9bb9d84a2b5407a68f
racka98 <bmkilaha@gmail.com>
2022-11-01 01:00:21 +0300

    update Billing flow to use Immutable Collections
    Signed-off-by: racka98 <bmkilaha@gmail.com>

--- common/billing/src/commonMain/kotlin/work/racka/reluct/common/billing/api/BillingApi.kt
+++ common/billing/src/commonMain/kotlin/work/racka/reluct/common/billing/api/BillingApi.kt
@@ -1,11 +1,12 @@
 package work.racka.reluct.common.billing.api
 
+import kotlinx.collections.immutable.ImmutableList
 import work.racka.reluct.common.billing.products.Product
 import work.racka.reluct.common.billing.products.ProductOffered
 import work.racka.reluct.common.model.util.Resource
 
 interface BillingApi {
-    suspend fun getProducts(filterProducts: List<ProductOffered> = listOf()): Resource<List<Product>>
+    suspend fun getProducts(filterProducts: List<ProductOffered> = listOf()): Resource<ImmutableList<Product>>
     suspend fun purchaseProduct(item: Product): Resource<Product>
 
     /** This return type is temporary. TODO: Change this later
--- common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/billing/impl/ManageCoffeeProductsImpl.kt
+++ common/domain/src/commonMain/kotlin/work/racka/reluct/common/domain/usecases/billing/impl/ManageCoffeeProductsImpl.kt
@@ -1,5 +1,6 @@
 package work.racka.reluct.common.domain.usecases.billing.impl
 
+import kotlinx.collections.immutable.ImmutableList
 import work.racka.reluct.common.billing.api.BillingApi
 import work.racka.reluct.common.billing.products.Product
 import work.racka.reluct.common.billing.products.Products
@@ -11,7 +12,7 @@ internal class ManageCoffeeProductsImpl(
 ) : ManageCoffeeProducts {
     override val api: BillingApi = billingApi
 
-    override suspend fun getAllProducts(): Resource<List<Product>> {
+    override suspend fun getAllProducts(): Resource<ImmutableList<Product>> {
         val products = listOf(Products.coffee1, Products.coffee2, Products.coffee3)
         return api.getProducts(filterProducts = products)
     }
