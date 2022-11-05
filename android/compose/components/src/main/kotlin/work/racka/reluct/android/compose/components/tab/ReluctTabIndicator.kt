094f7ef7df242963af3f2e2ed8ca0bc627c99a2b
racka98 <bmkilaha@gmail.com>
2022-11-05 20:55:09 +0300

    update tab indicator for DashboardTabBar
    Signed-off-by: racka98 <bmkilaha@gmail.com>

---
+++ android/compose/components/src/main/kotlin/work/racka/reluct/android/compose/components/tab/ReluctTabIndicator.kt
@@ -0,0 +1,46 @@
+package work.racka.reluct.android.compose.components.tab
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TabPosition
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.zIndex
+
+@Composable
+fun ReluctTabIndicator(
+    currentTabPosition: TabPosition,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .zIndex(1f)
+            .fillMaxSize()
+            .customTabIndicatorOffset(currentTabPosition)
+            .fillMaxSize()
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = CircleShape
+            )
+    )
+}
+
+fun Modifier.customTabIndicatorOffset(currentTabPosition: TabPosition): Modifier =
+    composed {
+        val currentTabWidth = currentTabPosition.width
+        val indicatorOffset by animateDpAsState(
+            targetValue = currentTabPosition.left,
+            animationSpec = tween()
+        )
+        fillMaxSize()
+            .wrapContentSize(Alignment.BottomStart)
+            .offset(x = indicatorOffset)
+            .width(currentTabWidth)
+    }
