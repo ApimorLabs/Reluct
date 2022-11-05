bcdff87dac175d7ed0a9b6ecb5edefdf9c7570f8
racka98 <bmkilaha@gmail.com>
2022-11-05 20:56:36 +0300

    update tab indicator for GoalsTabBar
    Signed-off-by: racka98 <bmkilaha@gmail.com>

--- android/compose/navigation/src/main/kotlin/work/racka/reluct/android/compose/navigation/toptabs/goals/GoalsTabBar.kt
+++ android/compose/navigation/src/main/kotlin/work/racka/reluct/android/compose/navigation/toptabs/goals/GoalsTabBar.kt
@@ -4,29 +4,27 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import kotlinx.collections.immutable.toImmutableList
+import work.racka.reluct.android.compose.components.tab.ReluctTabIndicator
 import work.racka.reluct.android.compose.components.tab.TabEntry
 import work.racka.reluct.android.compose.navigation.R
 
 @Composable
 fun GoalsTabBar(
-    tabPage: GoalsTabDestination,
+    tabPage: State<GoalsTabDestination>,
     onTabSelected: (tabPage: GoalsTabDestination) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     TabRow(
         modifier = modifier.width(250.dp),
-        selectedTabIndex = tabPage.ordinal,
+        selectedTabIndex = tabPage.value.ordinal,
         containerColor = Color.Transparent,
         indicator = { tabPositions ->
-            GoalsTabIndicator(
-                tabPositions = tabPositions.toImmutableList(),
-                tabPage = tabPage
-            )
+            ReluctTabIndicator(currentTabPosition = tabPositions[tabPage.value.ordinal])
         },
         divider = { }
     ) {
@@ -34,7 +32,7 @@ fun GoalsTabBar(
             title = stringResource(id = R.string.goals_active),
             textColor = getGoalsTabTextColor(
                 tabPage = GoalsTabDestination.Active,
-                selectedTabPage = tabPage
+                selectedTabPage = tabPage.value
             ),
             onClick = {
                 onTabSelected(GoalsTabDestination.Active)
@@ -44,7 +42,7 @@ fun GoalsTabBar(
             title = stringResource(id = R.string.goals_inactive),
             textColor = getGoalsTabTextColor(
                 tabPage = GoalsTabDestination.Inactive,
-                selectedTabPage = tabPage
+                selectedTabPage = tabPage.value
             ),
             onClick = {
                 onTabSelected(GoalsTabDestination.Inactive)
