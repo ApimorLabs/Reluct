bb619426a820c3595f0cb6db2c09108e8a32ad20
racka98 <bmkilaha@gmail.com>
2022-11-18 10:16:24 +0300

    update ProcessesProvider
    Signed-off-by: racka98 <bmkilaha@gmail.com>

--- desktop/app/src/desktopMain/kotlin/work.racka.reluct/ProcessesProvider.kt
+++ desktop/app/src/desktopMain/kotlin/work.racka.reluct/ProcessesProvider.kt
@@ -2,6 +2,7 @@ package work.racka.reluct
 
 import com.sun.jna.Native
 import com.sun.jna.platform.win32.WinDef
+import com.sun.jna.ptr.IntByReference
 import com.sun.jna.win32.StdCallLibrary
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collectLatest
@@ -16,7 +17,7 @@ class ProcessesProvider {
         scope.launch {
             flow<Int> {
                 var run = 0
-                while (run < 5) {
+                while (run < 10) {
                     WinSysUser32HWND.run()
                     emit(run)
                     delay(2000L)
@@ -41,16 +42,20 @@ object WinSysUser32HWND {
     @JvmStatic
     fun run() {
         val buffer = CharArray(1024 * 2)
-        User32.INSTANCE.GetWindowTextW(User32.INSTANCE.GetForegroundWindow(), buffer, 1024)
+        val lpdw = IntByReference(2)
+        val user32 = User32.INSTANCE
+        user32.GetWindowTextW(user32.GetForegroundWindow(), buffer, 1024)
+        val processId = user32.GetWindowThreadProcessId(user32.GetForegroundWindow(), lpdw)
         println("Active window title: " + Native.toString(buffer))
+        println("Thread ID: $processId")
     }
 
     interface User32 : StdCallLibrary {
         fun GetForegroundWindow(): WinDef.HWND?
-        fun GetWindowThreadProcessId()
+        fun GetWindowThreadProcessId(hWnd: WinDef.HWND?, lpdwProcessId: IntByReference?): Int
         fun GetWindowTextW(hWnd: WinDef.HWND?, lpString: CharArray?, nMaxCount: Int)
         companion object {
-            val INSTANCE = Native.loadLibrary(
+            val INSTANCE = Native.load(
                 "user32",
                 User32::class.java
             ) as User32
